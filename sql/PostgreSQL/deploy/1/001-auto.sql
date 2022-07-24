7654ecff7ca84b171806f3bd9a1f69aae32b68e0
Alastair Douglas (altreus) <altreus@altre.us>
2022-07-24 19:37:33 +0100

    Installation SQL from running dh.pl
    Note that this has created several directories, all with a 1 in, and
    each has a __VERSION file in it: this is the special DH version table
    used to track the current version of the database. The other file is
    just a bunch of CREATE TABLE and what-have-you that will deploy the
    schema at this version, i.e. 1

---
+++ sql/PostgreSQL/deploy/1/001-auto.sql
@@ -0,0 +1,56 @@
+--
+-- Created by SQL::Translator::Producer::PostgreSQL
+-- Created on Sun Jul 24 19:37:27 2022
+--
+;
+--
+-- Table: artist
+--
+CREATE TABLE "artist" (
+  "artistid" serial NOT NULL,
+  "name" text NOT NULL,
+  PRIMARY KEY ("artistid"),
+  CONSTRAINT "artist_name" UNIQUE ("name")
+);
+
+;
+--
+-- Table: cd
+--
+CREATE TABLE "cd" (
+  "cdid" serial NOT NULL,
+  "artistid" integer NOT NULL,
+  "title" text NOT NULL,
+  "year" timestamp,
+  PRIMARY KEY ("cdid"),
+  CONSTRAINT "cd_title_artistid" UNIQUE ("title", "artistid")
+);
+CREATE INDEX "cd_idx_artistid" on "cd" ("artistid");
+
+;
+--
+-- Table: track
+--
+CREATE TABLE "track" (
+  "trackid" serial NOT NULL,
+  "cdid" integer NOT NULL,
+  "title" text NOT NULL,
+  PRIMARY KEY ("trackid"),
+  CONSTRAINT "track_title_cdid" UNIQUE ("title", "cdid")
+);
+CREATE INDEX "track_idx_cdid" on "track" ("cdid");
+
+;
+--
+-- Foreign Key Definitions
+--
+
+;
+ALTER TABLE "cd" ADD CONSTRAINT "cd_fk_artistid" FOREIGN KEY ("artistid")
+  REFERENCES "artist" ("artistid") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
+
+;
+ALTER TABLE "track" ADD CONSTRAINT "track_fk_cdid" FOREIGN KEY ("cdid")
+  REFERENCES "cd" ("cdid") ON DELETE CASCADE ON UPDATE CASCADE DEFERRABLE;
+
+;
